28043bd3985012968d73612e9f5b6609a5380fc1
Szabolcs Légrádi <szabolcs.legradi@gmail.com>
2021-02-06 20:05:22 +0100

    chore: remove example HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style>
