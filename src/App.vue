80face371c19d72fa905819ea0988fbe341a729a
Szabolcs Légrádi <szabolcs.legradi@gmail.com>
2021-02-06 20:06:43 +0100

    feat: create todo list component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  
+  <TodoList />
 </template>
 
 <script setup>
+import TodoList from './components/TodoList.vue'
 </script>
 
 <style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="text" v-model="todo" placeholder="Teendő">
+  {{ todo }}
+</template>
+
+<script>
+export default {
+  setup () {
+    const todo = ''
+    
+    return {
+      todo
+    }
+  }
+}
+</script>
